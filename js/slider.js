9ffab6cb2d78956d49f21ebd900729a1f8157115
AlexandrCh04 <a.chertikhin0072@mail.ru>
2021-03-07 19:50:39 +0500

    .

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,13 @@
 const swiper = new Swiper('.main__slider', {
     speed: 500,
-    spaceBetween: 100,
+    spaceBetween: 120,
     slidesPerView: 3,
+    loop: true,
+    centeredSlides: true,
     autoplay: {
         delay: 1000,
         disableOnInteraction: false,
     },
-    loop: true,
-    noSwiping: false,
-    centeredSlides: true,
     breakpoints: {
         320: {
             slidesPerView: 1.5
